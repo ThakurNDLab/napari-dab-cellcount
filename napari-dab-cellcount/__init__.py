4b791ad3c635955adb00b8bd11dbdd4f3c460f7a
jyotirmay-neuro <101052218+jyotirmay-neuro@users.noreply.github.com>
2024-03-13 14:08:14 +0530

    __init__.py

--- napari-dab-cellcount/__init__.py
+++ napari-dab-cellcount/__init__.py
@@ -3,5 +3,5 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-from ._dock_widget import napari_cellcount_dock_widget
+from ._dock_widget import napari_experimental_provide_dock_widget
 from ._sample_data import napari_provide_sample_data
