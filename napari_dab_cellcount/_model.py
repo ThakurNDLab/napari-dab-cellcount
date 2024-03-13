f002f61a2370bfaddd09e05514f652d1ccc6826d
jyotirmay-neuro <101052218+jyotirmay-neuro@users.noreply.github.com>
2024-03-14 03:45:35 +0530

    Update _model.py

--- napari_dab_cellcount/_model.py
+++ napari_dab_cellcount/_model.py
@@ -1,84 +1,77 @@
 import torch
 import torch.nn as nn
-from torchvision.models import vgg16_bn
-
+from torchvision.models import resnet50
+from torch.nn import Conv2d, ConvTranspose2d, BatchNorm2d, ReLU, Sequential
 
 def double_conv(in_channels, out_channels):
     return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
-        nn.BatchNorm2d(out_channels),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
-        nn.BatchNorm2d(out_channels),
-        nn.ReLU(inplace=True)
+        Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+        BatchNorm2d(out_channels),
+        ReLU(inplace=True),
+        Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+        BatchNorm2d(out_channels),
+        ReLU(inplace=True)
     )
-
 
 def up_conv(in_channels, out_channels):
-    return nn.ConvTranspose2d(
-        in_channels, out_channels, kernel_size=2, stride=2
-    )
+    return ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
 
-
-class VGGUnet(nn.Module):
-    """Unet with VGG-11 (with BN) encoder.
-    """
-
+class ResNetUnet(nn.Module):
     def __init__(self, *, weights='DEFAULT', out_channels=1):
         super().__init__()
 
-        self.encoder = vgg11_bn(weights=weights).features
-        self.block1 = nn.Sequential(*self.encoder[:6])
-        self.block2 = nn.Sequential(*self.encoder[6:13])
-        self.block3 = nn.Sequential(*self.encoder[13:20])
-        self.block4 = nn.Sequential(*self.encoder[20:27])
-        self.block5 = nn.Sequential(*self.encoder[27:34])
+        # Load pretrained ResNet-50
+        pretrained_model = resnet50(weights=weights)
 
-        self.bottleneck = nn.Sequential(*self.encoder[34:])
-        self.conv_bottleneck = double_conv(512, 1024)
+        # Encoder
+        self.initial = Sequential(*list(pretrained_model.children())[:4]) # Initial layers
+        self.encoder1 = pretrained_model.layer1 # First residual block
+        self.encoder2 = pretrained_model.layer2 # Second residual block
+        self.encoder3 = pretrained_model.layer3 # Third residual block
+        self.encoder4 = pretrained_model.layer4 # Fourth residual block
 
+        # Bottleneck
+        self.bottleneck = double_conv(2048, 4096)
+
+        # Decoder
+        self.up_conv5 = up_conv(4096, 1024)
+        self.conv5 = double_conv(2048 + 1024, 1024)
         self.up_conv6 = up_conv(1024, 512)
-        self.conv6 = double_conv(512 + 512, 512)
+        self.conv6 = double_conv(1024 + 512, 512)
         self.up_conv7 = up_conv(512, 256)
-        self.conv7 = double_conv(256 + 512, 256)
-        self.up_conv8 = up_conv(256, 128)
-        self.conv8 = double_conv(128 + 256, 128)
-        self.up_conv9 = up_conv(128, 64)
-        self.conv9 = double_conv(64 + 128, 64)
-        self.up_conv10 = up_conv(64, 32)
-        self.conv10 = double_conv(32 + 64, 32)
-        self.conv11 = nn.Conv2d(32, out_channels, kernel_size=1)
+        self.conv7 = double_conv(512 + 256, 256)
+        self.up_conv8 = up_conv(256, 64)
+        self.conv8 = double_conv(256 + 64, 64)
+        self.final_conv = Conv2d(64, out_channels, kernel_size=1)
 
     def forward(self, x):
-        block1 = self.block1(x)
-        block2 = self.block2(block1)
-        block3 = self.block3(block2)
-        block4 = self.block4(block3)
-        block5 = self.block5(block4)
+        # Encoder
+        initial = self.initial(x)
+        enc1 = self.encoder1(initial)
+        enc2 = self.encoder2(enc1)
+        enc3 = self.encoder3(enc2)
+        enc4 = self.encoder4(enc3)
 
-        bottleneck = self.bottleneck(block5)
-        x = self.conv_bottleneck(bottleneck)
+        # Bottleneck
+        x = self.bottleneck(enc4)
+
+        # Decoder with skip connections
+        x = self.up_conv5(x)
+        x = torch.cat([x, enc4], dim=1)
+        x = self.conv5(x)
 
         x = self.up_conv6(x)
-        x = torch.cat([x, block5], dim=1)
+        x = torch.cat([x, enc3], dim=1)
         x = self.conv6(x)
 
         x = self.up_conv7(x)
-        x = torch.cat([x, block4], dim=1)
+        x = torch.cat([x, enc2], dim=1)
         x = self.conv7(x)
 
         x = self.up_conv8(x)
-        x = torch.cat([x, block3], dim=1)
+        x = torch.cat([x, enc1], dim=1)
         x = self.conv8(x)
 
-        x = self.up_conv9(x)
-        x = torch.cat([x, block2], dim=1)
-        x = self.conv9(x)
-
-        x = self.up_conv10(x)
-        x = torch.cat([x, block1], dim=1)
-        x = self.conv10(x)
-
-        x = self.conv11(x)
+        x = self.final_conv(x)
 
         return x
